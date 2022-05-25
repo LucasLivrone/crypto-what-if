c0b54a38865567058360f911e46e4e1b07462861
Lucas Livrone <lucas.a.livrone@tsgforce.com>
2022-05-25 12:59:46 -0300

    Update usd_scraper tests

--- src/utils/date_validator.py
+++ src/utils/date_validator.py
@@ -1,22 +1,27 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 # Coingecko free API support dates from May-2013 until present date
+# USD to ARS price is available from 2011 until previous month of present date
 
 
 def date_is_supported(date):
-    day = int(date[:2])
-    month = int(date[3:5])
-    year = int(date[-4:])
+    try:
+        day = int(date[:2])
+        month = int(date[3:5])
+        year = int(date[-4:])
+        input_date = datetime(year, month, day)
+    except ValueError:
+        return False
 
     present_date = datetime.now()
-    input_date = datetime(year, month, day)
+    last_month_date = present_date.replace(day=1) - timedelta(1)
 
     if year < 2013:
         return False
     elif year == 2013 and month < 5:
         return False
-    elif input_date > present_date:
+    elif input_date > last_month_date:
         return False
     else:
         return True
--- tests/test_usd_scraper.py
+++ tests/test_usd_scraper.py
@@ -1,7 +1,37 @@
 from src.utils.usd_scraper import get_usd_historical_price
+from datetime import datetime, timedelta
 
 
 def test_supported_date():
     date = "30-12-2017"
     price = get_usd_historical_price(date)
     assert price == 18.88
+
+
+def test_unsupported_past_date():
+    date = "30-12-2012"
+    result = get_usd_historical_price(date)
+    assert result == "Date is not supported"
+
+
+def test_unsupported_actual_date():
+    present_date = datetime.now()
+    present_date = present_date.strftime('%d-%m-%Y')  # tomorrow's date in DD-MM-YYYY format
+    result = get_usd_historical_price(present_date)
+    assert result == "Date is not supported"
+
+
+def test_unsupported_future_date():
+    present_date = datetime.now()
+    tomorrow_date = present_date + timedelta(1)
+    tomorrow_date = tomorrow_date.strftime('%d-%m-%Y')  # tomorrow's date in DD-MM-YYYY format
+    result = get_usd_historical_price(tomorrow_date)
+    assert result == "Date is not supported"
+
+
+def test_invalid_date():
+    date = ""
+    result = get_usd_historical_price(date)
+    assert result == "Date is not supported"
+
+
