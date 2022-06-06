5e2c6921dfb0803ba22f6038c1eee4985c3f4625
Lucas Livrone <lucas.a.livrone@tsgforce.com>
2022-06-06 11:13:27 -0300

    Added failure output to calculator.py

--- src/utils/calculator.py
+++ src/utils/calculator.py
@@ -12,6 +12,10 @@ def calculate(
     crypto_purchased_price = get_crypto_historical_price(crypto, date)
     crypto_actual_price = get_crypto_actual_price(crypto)
 
+    for value in [usd_purchased_price, crypto_purchased_price, crypto_actual_price]:
+        if isinstance(value, str):
+            return value
+
     usd_purchased_quantity = ars_quantity / usd_purchased_price
     crypto_purchased_quantity = usd_purchased_quantity / crypto_purchased_price
     purchased_crypto_actual_value = crypto_purchased_quantity * crypto_actual_price
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,7 +4,7 @@ from src.utils.crypto_api import get_crypto_actual_price
 
 # TODO: Mock API results
 
-def test_calculator():
+def test_supported_crypto_with_supported_date():
     ars_quantity = 1000
     crypto = "bitcoin"
     date = "30-12-2017"
@@ -17,6 +17,32 @@ def test_calculator():
         "purchased_crypto_actual_value": 0.0038887440865616398 * crypto_actual_price,
         "crypto_value_improvement": (crypto_actual_price * 100) / crypto_purchased_price
     }
+    actual_result = calculate(ars_quantity, crypto, date)
+    assert expected_result == actual_result
 
+
+def test_supported_crypto_with_unsupported_date():
+    ars_quantity = 1000
+    crypto = "bitcoin"
+    date = ""
+    expected_result = "Date is not supported"
+    actual_result = calculate(ars_quantity, crypto, date)
+    assert expected_result == actual_result
+
+
+def test_unsupported_crypto_with_unsupported_date():
+    ars_quantity = 1000
+    date = ""
+    crypto = ""
+    expected_result = "Date is not supported"
+    actual_result = calculate(ars_quantity, crypto, date)
+    assert expected_result == actual_result
+
+
+def test_unsupported_crypto_with_supported_date():
+    ars_quantity = 1000
+    date = "30-12-2017"
+    crypto = ""
+    expected_result = "Crypto is not supported"
     actual_result = calculate(ars_quantity, crypto, date)
     assert expected_result == actual_result
