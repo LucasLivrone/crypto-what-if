c6978146958c77868e4a1628c07fae693c2098d5
Lucas <lucaslivrone@gmail.com>
2022-07-13 10:22:59 -0300

    Fixed crypto_value_improvement_percentage issue

--- tests/integration_tests/test_evaluation.py
+++ tests/integration_tests/test_evaluation.py
@@ -15,12 +15,12 @@ async def test_supported_crypto_with_supported_date():
         "usd_purchased_quantity": 52.96610169491526,
         "crypto_purchased_quantity": 0.0038887440865616398,
         "purchased_crypto_actual_value": 0.0038887440865616398 * crypto_actual_price,
-        "crypto_value_improvement_percentage": (crypto_actual_price * 100) / crypto_purchased_price,
+        "crypto_value_improvement_percentage": ((crypto_actual_price * 100) / crypto_purchased_price)-100,
         "verbose_evaluation" : "If you had 1000 Argentinian pesos in 30-12-2017 you could have bought " \
                                "52.96610169491526 US Dollars and 0.0038887440865616398 bitcoin coins." \
                                "Today that quantity of bitcoin coins have a value of " +
                                str(0.0038887440865616398 * crypto_actual_price) + " US Dollars.The improvement on your " \
-                               "bitcoin investment would have been of " + str((crypto_actual_price * 100) / crypto_purchased_price) + " %"
+                               "bitcoin investment would have been of " + str(((crypto_actual_price * 100) / crypto_purchased_price)-100) + " %"
 
     }
 
